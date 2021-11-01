381aa76a5547a134d1a26820f1b695494c50c265
김유경(Yukyung Kim) <yklikesyou@gmail.com>
2021-11-01 23:24:38 +0900

    필요없는 주석제거

--- main.py
+++ main.py
@@ -4,8 +4,10 @@ import argparse
 from paperswithcode_handler import PapersWithCodeDataServicer
 from github_handler import GithubAPIServicer
 
+
 def parse_args():
-    parser = argparse.ArgumentParser(description='Paperswithcode와 Github의 환상적인 콜라보레이션')
+    parser = argparse.ArgumentParser(
+        description='Paperswithcode와 Github의 환상적인 콜라보레이션')
 
     parser.add_argument('--paperswithcode_json_path', dest='json_path', type=str,
                         default="./links-between-papers-and-code.json",
@@ -18,19 +20,18 @@ def parse_args():
     args = parser.parse_args()
     return args
 
+
 if __name__ == "__main__":
     args = parse_args()
-    
-    paperswithcode_servicer = PapersWithCodeDataServicer(json_path=args.json_path)
+
+    paperswithcode_servicer = PapersWithCodeDataServicer(
+        json_path=args.json_path)
     github_servicer = GithubAPIServicer(user_id=args.user_id, token=args.token)
-    # Get paper title list
-    # paper_list = servicer.print_paper_list(max_item_number=5)
-    # for paper_title in paper_list:
-    #     print(paper_title)
-        
+
     # Search papers with title
     query_title = "Sphere Generative Adversarial Network Based on Geometric Moment Matching"
-    paper_items = paperswithcode_servicer.search_with_title(query_title, only_official=True)
+    paper_items = paperswithcode_servicer.search_with_title(
+        query_title, only_official=True)
     print(f"{query_title}에 해당하는 github repo들을 확인해볼까요?")
     for paper_item in paper_items:
         try:
@@ -39,9 +40,3 @@ if __name__ == "__main__":
             print(f"{repo_url} 에 해당하는 github star는 {github_stars} 개입니다!")
         except RuntimeError as e:
             print(e)
-        
-    # # 만약 official repo에 해당하는 것만 보고 싶을 경우, only_official=True
-    # query_title = "Sphere Generative Adversarial Network Based on Geometric Moment Matching"
-    # paper_items = servicer.search_with_title(query_title, only_official=True)
-    # for paper_item in paper_items:
-    #     print(paper_item)
